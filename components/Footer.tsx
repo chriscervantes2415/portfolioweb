182fd25212be1f50c90779ba9bdb14b28c351b4d
chriscervantes2415 <139112002+chriscervantes2415@users.noreply.github.com>
2024-10-24 21:08:21 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <p className="text-sm py-2 text-center">
-      © Christian Jefferson Cervantes 2024444
+      © Christian Jefferson Cervantes 2024
     </p>
   );
 };
